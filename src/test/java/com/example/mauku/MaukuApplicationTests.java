ddc945a56185b18b0c8456d96d22e450b4fe5b86
haksutin <jenna.hakala97@gmail.com>
2024-04-21 20:08:26 +0300

    test fixed

--- src/test/java/com/example/mauku/MaukuApplicationTests.java
+++ src/test/java/com/example/mauku/MaukuApplicationTests.java
@@ -4,9 +4,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import com.example.mauku.web.CatController;
 import com.example.mauku.web.UserController;
 
+@SpringBootTest
 class MaukuApplicationTests {
 
 	@Autowired
